31d3f9405c4af74b393c25279602a7c6e3b72c85
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-10-23 21:22:37 -0300

    CLiente

--- src/SistemaBancario/CRUDCliente.java
+++ src/SistemaBancario/CRUDCliente.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CRUDCliente {
 
@@ -37,6 +39,57 @@ public class CRUDCliente {
             System.out.println(e.getMessage());
         }
         throw new RuntimeException("Cliente nao encontrado");
+    }
+
+    public static List<Cliente> buscarTudo(){
+        List<Cliente> clientes = new ArrayList<>();
+        try (Connection connection = ConexaoBanco.getConnection()){
+
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM tb_cliente");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String cpf = rs.getString("cpf");
+                clientes.add(new Cliente(id, nome, cpf));
+            }
+
+            return clientes;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        throw new RuntimeException("DEU RUIM BRO!");
+    }
+
+    public static void delete(int id){
+        try (Connection connection = ConexaoBanco.getConnection()) {
+
+            PreparedStatement ps = connection.prepareStatement("DELETE FROM tb_cliente where id = ?");
+            ps.setInt(1, id);
+            ps.execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void editar(int id, String nome, String cpf){
+
+        try (Connection connection = ConexaoBanco.getConnection()) {
+
+            PreparedStatement ps = connection.prepareStatement("UPDATE tb_cliente SET nome = ?, cpf = ? WHERE id = ?");
+            ps.setString(1, nome);
+            ps.setString(2, cpf);
+            ps.setInt(3, id);
+            ps.execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 
--- src/SistemaBancario/Main.java
+++ src/SistemaBancario/Main.java
@@ -17,45 +17,137 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cliente cliente = criarCliente();
-        CRUDCliente.salvar(cliente);
-        Conta conta = interacaoAddConta(cliente);
-        CRUDConta.salvar(conta);
-
-        System.out.println(cliente);
-        System.out.println(conta);
-
-        /*
+        int escolha;
 
         do {
-            System.out.print("1 - Criar\n2 - Editar\n3 - Buscar Por Número da Conta\n4 - Buscar Tudo\n5 - Deletar \n6 - Sair\nR: ");
-            int number = sc.nextInt();
+            System.out.println("Bem vindo ao sistema bancario, você deseja operar em\n1 - Clientes\n2 - Contas\n3 - Sair");
+            escolha = sc.nextInt();
+            sc.nextLine();
 
-            if (number == 1) {
-                interacaoAddConta();
-            } else if (number == 2) {
-                interacaoUpdate();
-            } else if (number == 3) {
-                interacaoBuscar();
-            } else if (number == 4) {
-                System.out.println(buscarTudo());
-            } else if (number == 5) {
-                interacaoDELETE();
-            } else {
-                System.exit(0);
+            switch (escolha) {
+                case 1:
+                    menuCliente();
+                    break;
+                case 2:
+                    //menuConta();
+                    //TODO Implementar
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
             }
         } while (true);
-         */
     }
 
-    private static Cliente criarCliente() {
-        System.out.print("CPF:");
+    private static void menuCliente() {
+        System.out.println("Bem vindo ao menu dos Clientes, você deseja");
+        System.out.print("1 - Criar\n2 - Editar\n3 - Buscar Por ID\n4 - Buscar Tudo\n5 - Deletar \n6 - Sair\nR: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+
+        if (number == 1) {
+            adicionarCliente();
+        } else if (number == 2) {
+            editarCliente();
+        } else if (number == 3) {
+            System.out.println(buscarCliente());
+        } else if (number == 4) {
+            System.out.println(buscarTodosOsClientes());
+        } else if (number == 5) {
+            deletarCliente();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    private static void adicionarCliente() {
+
+        System.out.println("Adicionar Novo Cliente");
+        System.out.println("Digite o nome do cliente: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite o cpf do cliente: ");
         String cpf = sc.nextLine();
 
-        System.out.print("Nome:");
+        CRUDCliente.salvar(new Cliente(nome, cpf));
+    }
+
+    private static void deletarCliente() {
+
+        System.out.println("Deletar Pelo número");
+        System.out.println("Digite o número que você quer deletar: ");
+        CRUDCliente.delete(sc.nextInt());
+
+    }
+
+    private static List<Cliente> buscarTodosOsClientes() {
+        return CRUDCliente.buscarTudo();
+    }
+
+    private static Cliente buscarCliente() {
+
+        System.out.println("Digite o ID do cliente que deseja buscar: ");
+        try {
+            return CRUDCliente.buscarPorId(sc.nextInt());
+        } catch (Exception e) {
+            System.out.println("ID Inexistente");
+            return null;
+        }
+    }
+
+    private static void editarCliente() {
+
+        System.out.println("Digite o ID que deseja editar: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite o novo nome do cliente: ");
         String nome = sc.nextLine();
+
+        System.out.println("Digite o novo cpf do cliente: ");
+        String cpf = sc.nextLine();
+
+        CRUDCliente.editar(id, nome, cpf);
+
+    }
+
+     /*
+
+      private static void menuConta() {
+
+        System.out.println("Bem vindo ao menu das Contas, você deseja");
+        System.out.print("1 - Criar\n2 - Editar\n3 - Buscar Por Número da Conta\n4 - Buscar Tudo\n5 - Deletar \n6 - Sair\nR: ");
+        int number = sc.nextInt();
+
+        if (number == 1) {
+            interacaoAddConta();
+        } else if (number == 2) {
+            interacaoUpdate();
+        } else if (number == 3) {
+            interacaoBuscar();
+        } else if (number == 4) {
+            System.out.println(buscarTudo());
+        } else if (number == 5) {
+            interacaoDELETE();
+        } else {
+            System.exit(0);
+        }
+
+    }
+
+
+    private static Cliente criarCliente() {
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
         return new Cliente(nome, cpf);
     }
+
 
     private static Conta interacaoAddConta(Cliente titular) {
         System.out.print("Número Conta: ");
@@ -71,7 +163,7 @@ public class Main {
         return new Conta(numeroConta, titular, saldo, limite);
     }
 
-     /*
+
     private static void interacaoDELETE() {
         System.out.println("Deletar Pelo número");
         System.out.println("Digite o número que você quer deletar");
